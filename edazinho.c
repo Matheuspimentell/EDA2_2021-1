e67a2a421d3620a642d7bfbf0fa6d3b2c9eedaf5
Matheus Pimentel Leal <62526025+Matheuspleal@users.noreply.github.com>
2021-10-19 12:36:05 -0300

    Implementação da sondagem

--- edazinho.c
+++ edazinho.c
@@ -3,106 +3,203 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 //Variáveis fixadas no início do jogo
 long long int LINHA_INCIAL, COLUNA_INICIAL, PONTOS_INCIAL; //long long int suporta dados entre -2^63 e 2^63 - 1
-unsigned int TURNOS, tamanhoHash; // unsigned int para armazenar dado da quantidade de turnos
-long MAX = ((pow(2,21))-1), MIN = -(pow(2,21)); // MAX = 2^21, MIN = -2^21
-
-//Para gerar números aleatórios entre MAX e MIN
-#define geraPontuacao() \
-    ((rand()%(MAX-MIN))+MIN);
-
-//Gerar código hash para saber posições disponíveis para sondagem
-#define geraHash(linha, coluna) \
-    (abs((linha+coluna)%tamanhoHash));
+unsigned int TURNOS; // unsigned int para armazenar dado da quantidade de turnos: TURNOS varia de 10 à 2^31
 
 //Lista simplemente encadeada com todos os edazinhos
 typedef struct edazinhos{
-    //Linha e coluna do edazinho atual
-    long long int linha, coluna;
-    //Ponteiro para o próximo edazinho
-    struct edazinhos *prox;
+    long long int linha, coluna; //Linha e coluna do edazinho atual
+    int posicoesSondadas; // Varia entre 0 e 8. 8 = Todas as posições em volta foram sondadas | 0 = nenhuma posição foi sondada
+    struct edazinhos *prox; //Ponteiro para o próximo edazinho
 }edazinhos;
 
-//Implementar fila de prioridade para as células sondadas com base na pontuação (Binary heap)
-typedef struct filaDominacao{
-    //Linha e coluna da célula sondada
-    long long int linha, coluna;
-    //Ponteiro para a próxima célula já sondada
-    struct filaDominacao *prox;
-}filaDominacao;
+//Cada célula do jogo
+typedef struct celula{
+    long long int linha, coluna, pontos;
+}celula;
 
-//Lógica para sondar uma posição
-edazinhos *sondar(edazinhos *listaEdazinhos, long long int Linha, long long int Coluna){
-    //Checar posições válidas para sondagem
-    return listaEdazinhos;
+/*
+ *A implementação de uma HEAP é feita por meio de vetores. Onde não se usa o índice 0 do vetor.
+ *A posição dos filhos de um nó da HEAP é dada por: 2*k e 2*k+1, respectivamente. Onde k é a posição do nó pai
+ *Igualmente, pode-se encontrar a posição do nó pai com o resultado inteiro de k/2. Onde k é a posição do nó filho
+ *A inserção deve começar pelo índice 1
+ *Usar a implementação de uma MAXHeap
+ *Utilizando o índice 0 para a última posição removida
+ * */
+celula heap[301];
+int _sizeHeap = 0;
+
+//Funções essenciais da Heap
+int Pai(int index){return (index/2);}
+
+int FilhoEsquerda(int index){return (index*2);}
+
+int FilhoDireita(int index){return ((index*2)+1);}
+
+//Troca simples entre 2 elementos da heap
+void HeapSwap(int indexPai, int indexFilho){
+    celula aux;
+    aux = heap[indexPai];
+    heap[indexPai] = heap[indexFilho];
+    heap[indexFilho] = aux;
 }
 
-//Lógica para dominar uma posição
-edazinhos *dominar(edazinhos *listaEdazinhos, long long int Linha, long long int Coluna){
-    
-    //Inserir um edazinho no final da lista de edazinhos
-    edazinhos *aux, *novoEdazinho;
-    
-    //Aloca memória para +1 edazinho e seta a sua linha e coluna
-    novoEdazinho = (edazinhos *) malloc(sizeof(edazinhos));
-    novoEdazinho->linha = Linha;
-    novoEdazinho->coluna = Coluna;
-    novoEdazinho->prox = NULL;
-    
-    //Faz o ponteiro auxiliar ir até o final da lista de edazinhos
-    aux = listaEdazinhos;
-    while(aux->prox != NULL){
-        aux = aux->prox;
+void insertHeap(celula nova){
+
+    if(_sizeHeap == 300){
+        printf("Overflow! nao foi possivel inserir na heap");
+        return;
     }
 
-    //Insere o novo edazinho no final da lista
-    aux->prox = novoEdazinho;
+    //Incrementa o tamanho da heap e insere a nova celula na primeira posição disponível
+    _sizeHeap++;
+    int i = _sizeHeap;
+    heap[i] = nova;
 
-    return listaEdazinhos;
+    //Checar e consertar a Heap, caso necessário
+    while(i > 1 && heap[Pai(i)].pontos < heap[i].pontos){
+        HeapSwap(Pai(i), i);
+        i = Pai(i);
+    }
+}
+
+//Sondar a diagonal superior direita do edazinho
+void sondar(edazinhos *playerList){
+
+    //Linha e coluna a serem sondadas
+    long long int linhaSondar;
+    long long int colunaSondar;
+
+    //Decidir qual posição sondar
+    if(playerList->posicoesSondadas == 8){
+        printf("Todas as posições em volta deste player foram sondadas.\n");
+        //Avançar para o próximo player
+        playerList = playerList->prox;
+        //Se houver mais players, sondar. Senão, sair
+        if(playerList!=NULL){
+            sondar(playerList);
+        } else {
+            return;
+        }
+    } else if (playerList->posicoesSondadas == 0){
+        //Sondar a diagonal direita superior
+        linhaSondar = playerList->linha+1;
+        colunaSondar = playerList->coluna+1;
+    } else if (playerList->posicoesSondadas == 1){
+        //Sondar a diagonal esquerda inferior
+        linhaSondar = playerList->linha-1;
+        colunaSondar = playerList->coluna-1;
+    } else if (playerList->posicoesSondadas == 2){
+        //Sondar a diagonal direita inferior
+        linhaSondar = playerList->linha-1;
+        colunaSondar = playerList->coluna+1;
+    } else if (playerList->posicoesSondadas == 3){
+        //Sondar a diagonal esquerda superior
+        linhaSondar = playerList->linha+1;
+        colunaSondar = playerList->coluna-1;
+    } else if (playerList->posicoesSondadas == 4){
+        //Sondar a posicao acima
+        linhaSondar = playerList->linha+1;
+        colunaSondar = playerList->coluna;
+    } else if (playerList->posicoesSondadas == 5){
+        //Sondar a posição abaixo
+        linhaSondar = playerList->linha-1;
+        colunaSondar = playerList->coluna+1;
+    } else if (playerList->posicoesSondadas == 6){
+        //Sondar a posição da direita
+        linhaSondar = playerList->linha;
+        colunaSondar = playerList->coluna+1;
+    } else if (playerList->posicoesSondadas == 7){
+        //Sondar a posição da esquerda
+        linhaSondar = playerList->linha;
+        colunaSondar = playerList->coluna-1;
+    }
+
+    //Impressão do comando
+    printf("sondar %lld %lld\n", linhaSondar, colunaSondar);
+    
+    //Esperar a resposta do árbitro
+    long long int l, c, p;
+    char sondagem[8];
+    scanf("%s %lld %lld %lld", sondagem, &l, &c, &p);
+
+    //Inserir a nova célula na heap
+    celula nova = {l,c,p};
+    insertHeap(nova);
+    playerList->posicoesSondadas++;
+
+    //Chamada recursiva para a função de sondagem
+    playerList = playerList->prox;
+    if(playerList!=NULL){
+        sondar(playerList);
+    }
+    return;
 }
 
 //Implementação do jogador: Reagir às entradas do árbitro
 int main(){
     
-    scanf("%u", &TURNOS);    
+    //Coloca um valor qualquer na posição 0 da heap
+    heap[0].linha = -1;
+    heap[0].coluna = -1;
+    heap[0].pontos = -1;
+    
+    //Leitura inicial do jogo
+    scanf("%lld %lld %lld %u", &LINHA_INCIAL, &COLUNA_INICIAL, &PONTOS_INCIAL, &TURNOS);
 
-    tamanhoHash = (9+(5*(TURNOS-1)));
+    //Variáveis que armazenam o total de pontos obtidos e de edazinhos em campo
+    long long int totalPontos = 0,totalEdazinhos = 0;
+    
+    //Primeiro edazinho em campo
+    struct edazinhos *listaEdazinhos;
+    listaEdazinhos = (edazinhos *) malloc(sizeof(edazinhos));
+    listaEdazinhos->linha = LINHA_INCIAL;
+    listaEdazinhos->coluna = COLUNA_INICIAL;
+    listaEdazinhos->posicoesSondadas = 0;
+    listaEdazinhos->prox = NULL;
+    totalEdazinhos++;
+    totalPontos += PONTOS_INCIAL;
+    unsigned int i = 0;
 
-    if(tamanhoHash%2 == 0){
-        tamanhoHash++;
-    }
+    //game loop
+    do{
+        printf("======Turno %d\n", i);
+        
+        /*
+         *Lógica por turno:
+         *
+         *1° - Dominar uma posição
+         *
+         *2° - Sondar as posições baseado em algum algoritimo
+         *
+         *3° - fimturno
+         * */
 
-    long long int linhaI = geraPontuacao();
-    long long int colunaI = geraPontuacao();
+        if(_sizeHeap > 0){
+            printf("pode dominar!\nHeap:\n");
+            int i = 1;
+            while(i <= _sizeHeap){
+                printf("l:%lld, c:%lld, p:%lld\n", heap[i].linha, heap[i].coluna, heap[i].pontos);
+                i++;
+            }
+        }
 
-    long long int linhaJ = linhaI;
-    long long int colunaJ = colunaI;
+        //ponteiro auxiliar para a lista de edazinhos e suas posições
+        struct edazinhos *aux = listaEdazinhos;
+        sondar(aux);
 
-    int i = 1;
-    while(i <= 30){
-        printf("linha:%lld coluna:%lld\n", linhaI, colunaI);
-        unsigned int teste = geraHash(linhaI, colunaI);
-        printf("hashKey:%u\n\n", teste);
-        linhaI++;
-        colunaI++;
+        //Acabando o turno, invoca o comando "fimturno"
+        printf("fimturno\n");
+        fflush(stdout);
         i++;
-    }
+    }while(i < TURNOS);
 
-
-    printf("===================================================\n");
-
-    int j = 1;
-    while(j <= 30){
-        printf("linha:%lld coluna:%lld\n", linhaJ, colunaJ);
-        unsigned int teste = geraHash(linhaJ, colunaJ);
-        printf("hashKey:%u\n\n", teste);
-        linhaJ--;
-        colunaJ--;
-        j++;
-    }
-
+    printf("Total de edazinhos: %lld\n", totalEdazinhos);
+    printf("Pontuacao final: %lld\n", totalPontos);
+    free(listaEdazinhos);
     return 0;
 
 }
